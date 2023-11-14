392f7836458a94b66c3a073c1e00b30f6252a6c9
MarinProdan <150788912+MarinProdan@users.noreply.github.com>
2023-11-14 11:36:13 +0100

    Update lala.py

--- lala.py
+++ lala.py
@@ -7,3 +7,12 @@ for i in range(len(numbers)-1):
         numbers[i] = numbers[i+1]
         numbers[i+1] = tmp
 print(numbers)
+
+import math
+player = [2,5]
+enemy= [3,3]
+
+a=abs(player[0] - enemy[0])
+b=abs(player[1] - enemy[1])
+c=math.sqrt (a**b + b**2)
+print(c)
