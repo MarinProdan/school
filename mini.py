4a36d1e6fff1f9796d877b295fea06abd78891ed
MarinProdan <150788912+MarinProdan@users.noreply.github.com>
2024-02-05 17:48:54 +0100

    Update mini.py

--- mini.py
+++ mini.py
@@ -1,11 +1,13 @@
 import random
 
-w, h, mines  = 10, 5, 8
+w, h, mines  = 10, 5, 3
 
 
 #gene field
 field=[[0 for y in range(h)]for x in range (w)]
-  
+if mines > w * h:
+  mines=0
+  print(" Error: too many mines")
 for i in range(mines):
     while True:
      rx = random.randint(0, w - 1)
@@ -13,6 +15,22 @@ for i in range(mines):
      if field[rx][ry] != "M":
       field[rx][ry] = "M"
      break
+
+deltas=((-1,-1),(0,-1),(1,-1), (-1,0),(1,0),(-1,1),(0,1),(1,1))
+for y in range(h):
+      for x in range (w):
+         if field[x][y]=="M":
+        
+          #print("mina na [{},{}]".format (x,y))
+          for delta in deltas:
+             nx = x + delta[0]
+             ny = y + delta[1]
+             if nx >= 0 and nx<w and ny >= 0 and ny< h and field [nx][ny] !="M":
+                field[nx][ny] +=1
+           
+            
+        
+            
 for y in range (h):
     for x in range (w):
       print(field[x][y], end='')
