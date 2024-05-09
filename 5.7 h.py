f7f77104e5b95a126096b34d08cf3b167e8e0f2d
MarinProdan <150788912+MarinProdan@users.noreply.github.com>
2024-05-09 16:58:52 +0200

    Create 5.7 h.py

---
+++ 5.7 h.py
@@ -0,0 +1,14 @@
+
+cnt=0
+iter=0
+for a in range(10):
+    for b in range(10):
+        for c in range(10):
+            for d in range(10):
+                if a < b and b < c and c < d and a + b + c + d==13:
+                    print(f"kombinace:[{a}{b}{c}{d}]")
+                    cnt+=1
+                iter+=1
+print(f"{cnt}/{iter}")
+
+           
